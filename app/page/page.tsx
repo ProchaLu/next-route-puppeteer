fb41f8ae479eba1478a036255a7503f76703e076
Lukas Prochazka <prochazka.lu@gmail.com>
2023-08-21 09:53:35 +0200

    Delete client component

--- app/page/page.tsx
+++ app/page/page.tsx
@@ -1,10 +1,20 @@
-import PageComponent from './PageComponent';
+import Image from 'next/image';
 
-export default function PagePage() {
+function delay() {
+  return new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  });
+}
+
+export default async function PagePage() {
+  await delay();
   return (
     <div>
       <h1>PAGE</h1>
-      <PageComponent />
+      <h2>Page Component</h2>
+      <Image src="/images/image.jpg" alt="image" width={300} height={300} />
     </div>
   );
 }
