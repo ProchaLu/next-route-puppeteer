6646e176c8f9c9536e406b5c2adf7e039e4f1bd4
Lukas Prochazka <prochazka.lu@gmail.com>
2023-09-05 10:12:53 +0200

    Add session display

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { cookies } from 'next/headers';
+
 export default function Home() {
+  const session = cookies().get('session')?.value;
   return (
     <main>
       <h1>TEST</h1>
+      <div>Session: {session}</div>
     </main>
   );
 }
